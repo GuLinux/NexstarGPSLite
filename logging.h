6198b42ec7f75036368e70967891b4badbdb4116
Marco Gulino <marco@gulinux.net>
2019-05-11 19:20:49 +0100

    Function tracer

--- logging.h
+++ logging.h
@@ -18,5 +18,25 @@
 #define VERBOSE(s) LOG(verbose, s)
 #define VERBOSE_F(s, ...) LOG_F(verbose, s, __VA_ARGS__)
 
+class DebugLog {
+public:
+    DebugLog(const char *file, int line, const char *func) : file(file), line(line), func(func) {
+        VERBOSE_F("[DEBUG] ENTER %s (%s:%d)", func, file, line);
+    }
+    ~DebugLog() {
+        VERBOSE_F("[DEBUG] EXIT %s (%s:%d)", func, file, line);
+    }
+private:
+    const char *file;
+    int line;
+    const char *func;
+};
+
+#ifdef TRACE_FUNCTIONS
+#define DEBUG_F DebugLog __dbg_log(__FILE__, __LINE__, __FUNCTION__);
+#else
+#define DEBUG_F
+#endif
+
 
 // vim: set shiftwidth=2 tabstop=2 expandtab:indentSize=2:tabSize=2:noTabs=true:
